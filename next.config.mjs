c7cfc97d0f2030aa3d38b549a31daaa7be0b7fb0
Aziz <atamirzaev.aziz@gmail.com>
2024-08-22 12:38:18 +0600

    Update config and add deploy script for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { defineConfig } from 'next';
 
-export default nextConfig;
+export default defineConfig({
+    output: 'export',
+    basePath: 'mapbox-task',
+    assetPrefix: 'mapbox-task',
+});
